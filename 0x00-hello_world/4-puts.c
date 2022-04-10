54c74ee244793e9ac3bd67e10295b750d2a7c389
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-10 21:26:12 +0000

    4inputs task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main function begins
+ *
+ * Return: function ends
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
