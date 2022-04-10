49a94091e418d513a15364b9b52a60b2b513ff84
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-10 21:53:19 +0000

    Print with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return: Function ends
 */
-init main(void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
