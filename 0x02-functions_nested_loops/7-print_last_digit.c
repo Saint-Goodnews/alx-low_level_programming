d1e446e231c7848b5ca20f9f4283043254eb6553
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-12 22:49:13 +0000

    prints last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,9 @@ int print_last_digit(int n)
 {
 	int last;
 
-	last = n % 10;
-	
+	last = n % 10;	
 	if (last < 0)
-	last = last * -1;
-	_putchar(last + '0');		
+		last = last * -1;
+	_putchar(last + '0');
 	return (last);
 }
