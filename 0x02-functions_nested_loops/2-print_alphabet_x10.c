e8f05a5c42e3758c07b7658fd1465660744f7859
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-12 20:18:36 +0000

    alphabet by 10 times.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,16 +10,13 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
-	
 	while (i < 10)
 	{
 		c = 'a';
 		while (c <= 'z')
 		{
-			
 			_putchar(c);
 			c++;
-		
 		}
 		_putchar('\n');
 		i++;
