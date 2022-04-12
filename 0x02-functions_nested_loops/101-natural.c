f99799eaa3292f6a4802f01ddff1051d338cad47
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-12 23:23:19 +0000

    printing n time

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,16 +10,16 @@
 int main(void)
 {
 	int c = 0;
-	int sum = 0;
-	
-	while (c < 1024)
+	int sum = 0;	
+
+while (c < 1024)
+{
+	if (c % 3 == 0 || c % 5 == 0)
 	{
-		if (c % 3 == 0 || c % 5 == 0)
-		{
-			sum += c;
-		}
-		c++;
+		sum += c;
 	}
-	printf("%i\n", sum);
-	return (0);
+	c++;
+}
+printf("%i\n", sum);
+return (0);
 }
