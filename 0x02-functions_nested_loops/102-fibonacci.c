c91ef624a643f28afea6090a7fdfbbe241e495a0
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-12 23:52:31 +0000

    first 50 fibo number

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,7 +5,7 @@
  *5 below 1024 (excluded), followed by a new line
  *
  * Return: 0
- */
+*/
 int main(void)
 {
 int i = 0;
@@ -17,11 +17,11 @@ while (i < 50)
 	a = b;
 	b = next;
 	printf("%lu", next);	
-	if (i < 49)
-	{
-		printf(", ");
-	}
-	i++;
+if (i < 49)
+{
+	printf(", ");
+}
+i++;
 }
 putchar('\n');
 return (0);
