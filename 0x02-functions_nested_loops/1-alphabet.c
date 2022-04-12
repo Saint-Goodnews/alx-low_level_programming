9163e81cba8910fcb2395bdebeee7051dce013cb
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-12 20:04:20 +0000

    print alphabet this

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,11 @@
 void print_alphabet(void)
 {
 	char c = 'a';
-
-		while (c <= 'z')
-			{
-				_putchar(c);
-				c++;
-			}
-		_putchar('\n');
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 
 }
