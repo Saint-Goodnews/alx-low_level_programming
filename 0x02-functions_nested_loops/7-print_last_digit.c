7aa0d4aa4e5f5c3f10f33ad5051133bbce510c76
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-13 00:19:39 +0000

    print Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,15 +4,15 @@
  *@n:  is the int that will use for the argument of the function
  *
  * Return: integer value
- */
+*/
 
 int print_last_digit(int n)
 {
 	int last;
-	
-	last = n % 10;
-	if (last < 0)
-		last = last * -1;
-	_putchar(last + '0');
-	return (last);
+
+last = n % 10;
+if (last < 0)
+	last = last * -1;
+_putchar(last + '0');
+return (last);
 }
