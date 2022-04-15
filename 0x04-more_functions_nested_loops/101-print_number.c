2d05ffc525493f8b5cc9399cc1580bd895cf2923
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-15 21:59:13 +0000

    More function task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,9 +13,9 @@ void print_number(int n)
 		_putchar(45);
 		i = -i;
 	}
-	if (i / 10)
+	if (i/10)
 	{
-		print_number(i / 10);
+		print_number(i/10);
 	}
-	_putchar(i % 10 + '0');
+	_putchar(i%10 + '0');
 }
