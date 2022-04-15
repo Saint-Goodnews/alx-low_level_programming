6edd6162ebfc0842499b67a6d5fabdd3a6f1b8c3
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-15 22:03:29 +0000

    More function task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,15 @@
 void print_number(int n)
 {
 	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar(45);
 		i = -i;
 	}
-	if (i/10)
+	if (i / 10)
 	{
-		print_number(i/10);
+		print_number(i / 10);
 	}
-	_putchar(i%10 + '0');
+	_putchar(i % 10 + '0');
 }
