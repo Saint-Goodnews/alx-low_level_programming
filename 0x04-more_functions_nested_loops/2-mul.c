d33595bb25014cc9ec01e13e069206bf21d0124a
Saint-Goodnews <101555716+Saint-Goodnews@users.noreply.github.com>
2022-04-15 23:30:31 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,13 +3,13 @@
  * mul - multiplies two integers
  * @a: multiplying
  * @b: multiplier
- * 
+ *
  * Return: the result of mutiplication
  */
 int mul(int a, int b)
 {
 	int result;
-	
+
 	result = a * b;
 	return (result);
 }
