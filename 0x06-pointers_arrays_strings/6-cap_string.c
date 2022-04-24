b05080908c512de43ba1ebdcf025498158e9010a
Saint-Goodnews <ajilogoodnews@gmail.com>
2022-04-24 21:07:08 +0000

    Task 6 pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *s)
 {
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-	
+
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
 	count++;
@@ -20,7 +20,6 @@ char *cap_string(char *s)
 			if (*(s + count) == sep_words[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
